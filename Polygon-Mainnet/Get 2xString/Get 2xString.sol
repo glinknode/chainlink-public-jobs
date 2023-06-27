43392a87eeab3fbd0d92ff990b11081e250af62e
Mathias | GLINK <71456816+Malir1988@users.noreply.github.com>
2023-06-27 06:08:31 +0200

    Add files via upload

---
+++ Polygon-Mainnet/Get 2xString/Get 2xString.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+// https://glink.solutions
+// Discord=https://discord.gg/a69JjGd3y6
+
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * THIS EXAMPLE USES UN-AUDITED CODE.
+ * DO NOT USE THIS CODE IN PRODUCTION.
+ */
+
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract getStringTemplate is ChainlinkClient {
+  using Chainlink for Chainlink.Request;
+
+  string public stringVariable;
+
+  bytes32 private externalJobId;
+  uint256 private oraclePayment;
+
+  constructor(
+  ) {
+    setChainlinkToken(0xb0897686c545045aFc77CF20eC7A532E3120E0F1);
+    setChainlinkOracle(0xbac0638773782e9130DfeAF59a5b27bBc49a7007);
+    externalJobId = "09d34f01138043c9a9756ee95b1b2c1a";
+    oraclePayment = ((25 * LINK_DIVISIBILITY) / 100); // n * 10**18
+  }
+
+  function requestString(
+  )
+    public
+  {
+    Chainlink.Request memory req = buildChainlinkRequest(externalJobId, address(this), this.fulfillString.selector);
+    req.add("get", "https://your_API_endpoint_url");
+    req.add("path1", "data,results");
+    sendOperatorRequest(req, oraclePayment);
+  }
+
+  event RequestFulfilled(bytes32 indexed requestId, string indexed stringVariable);
+
+  function fulfillString(bytes32 requestId, string calldata _stringVariable)
+    public
+    recordChainlinkFulfillment(requestId)
+  {
+    emit RequestFulfilled(requestId, _stringVariable);
+    stringVariable = _stringVariable;
+  }
+
+}
